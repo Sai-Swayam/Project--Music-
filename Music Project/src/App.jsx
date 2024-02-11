14a15adc4930bfb6e3a2804e83075d1834d7eda4
Sai Swayam <saiswayamshree19@gmail.com>
2024-02-11 21:17:42 +0530

    First commit

---
+++ Music Project/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Rack from "./components/Rack";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Hello</h1> */}
+      <div className="blur-vignette"></div>
+      {/* <div className="comp">
+        <img src="./src/assets/comp.jpg" width="300px" alt="" />
+      </div> */}
+      <Rack />
+    </>
+  );
+}
+
+export default App;
---
+++ Music Project/src/components/Rack.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rack = () => {
+  return (
+    <div>
+      <Patch />;
+    </div>
+  );
+};
+
+export default Rack;
