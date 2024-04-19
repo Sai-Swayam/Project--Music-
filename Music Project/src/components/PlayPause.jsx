cb4e8000eb0eab9db09bd8895716d41f92f669ab
Sai Swayam Shree <saiswayamshree19@gmail.com>
2024-04-20 01:05:33 +0530

    fixed mute unmute problems

--- Music Project/src/components/PlayPause.jsx
+++ Music Project/src/components/PlayPause.jsx
@@ -1,48 +1,53 @@
-import React from 'react'
-import { useState } from 'react'
-import './PlayPause.css'
-import * as Tone from 'tone'
+import React from "react";
+import { useState, useContext } from "react";
+
+import Context from "../context/Context";
+
+import "./PlayPause.css";
+import * as Tone from "tone";
 
 const PlayPause = () => {
-  
-  const [toneStarted, setToneStarted] = useState(false)
-  const [playing, setPlaying] = useState(false)
-  const [setup, setSetup] = useState(false)
+  const { samples, setSamples } = useContext(Context);
+  const [toneStarted, setToneStarted] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [setup, setSetup] = useState(false);
 
   async function goHandler(event) {
-      if(!toneStarted) await Tone.start()
-      setToneStarted(true)
-      setPlaying(!playing)
-      if(playing) return Tone.Transport.stop()
-      
-      if(!setup){
-          var kick = new Tone.Player("/kick-acoustic01.wav").toDestination()
-          var snare = new Tone.Player("/snare-acoustic01.wav").toDestination()
-          await Tone.loaded()
+    if (!toneStarted) await Tone.start();
+    setToneStarted(true);
+    setPlaying(!playing);
+    if (playing) return Tone.Transport.stop();
 
-          // play a note every quarter-note
-          new Tone.Loop(time => {
-              kick.start(time)
-          }, "4n").start(0)
+    if (!setup) {
+      var kick = new Tone.Player("/kick-acoustic01.wav").toDestination();
+      var snare = new Tone.Player("/snare-acoustic01.wav").toDestination();
+      await Tone.loaded();
 
-          // play another note every off quarter-note, by starting it "8n"
-          new Tone.Loop(time => {
-              snare.start(time)
-          }, "4n").start("8n")
+      // play a note every quarter-note
+      new Tone.Loop((time) => {
+        kick.start(time);
+      }, "4n").start(0);
 
-          // Tone.Transport.bpm.rampTo(50, 0.2);
-          setSetup(true)
-      }
-  
-      Tone.Transport.bpm.value =  150;
-      Tone.Transport.start()
+      // play another note every off quarter-note, by starting it "8n"
+      new Tone.Loop((time) => {
+        snare.start(time);
+      }, "4n").start("8n");
+
+      // Tone.Transport.bpm.rampTo(50, 0.2);
+      setSetup(true);
+    }
+
+    Tone.Transport.bpm.value = 150;
+    Tone.Transport.start();
   }
 
   return (
-    <div className='playpause'>
-        <button className="button" onClick={goHandler}>{playing ? "STOP" : "PLAY"}</button>
+    <div className="playpause">
+      <button className="button" onClick={goHandler}>
+        {playing ? "STOP" : "PLAY"}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default PlayPause
+export default PlayPause;
--- Music Project/src/components/Step.jsx
+++ Music Project/src/components/Step.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Context from "../context/Context";
 import "./Step.css";
 
-
 const Step = (props) => {
+  useEffect(() => {});
+
+  // ----------------------------------------------------------
+
   const [mute, setMute] = useState(props.step.mute);
   const { samples, setSamples } = useContext(Context);
 
@@ -21,27 +24,31 @@ const Step = (props) => {
   //-----------------------------------------------------------
 
   const toggleMute = () => {
-    if(samples[props.id].StepArray[props.index] === 0)
-      {
-        let tempSample = samples;
-        tempSample[props.id].StepArray[props.index] = 1;
-        setMute(tempSample);
-        console.log("unmuted " + props.index);
-      }
-      else{
-        let tempSample = samples;
-        tempSample[props.id].StepArray[props.index] = 0;
-        setMute(tempSample);
-        console.log("muted " + props.index); 
-      }
-  }
+    if (samples[props.id].StepArray[props.index] === 0) {
+      console.log(samples[props.id].StepArray[props.index]);
+      let tempSample = samples;
+      tempSample[props.id].StepArray[props.index] = 1;
+      setSamples(tempSample);
+      setMute(1);
+      console.log("muted " + props.index);
 
-  // console.log(samples[props.id].StepArray[props.index]);
+      console.log(samples[props.id].StepArray[props.index]);
+    } else {
+      console.log(samples[props.id].StepArray[props.index]);
+      let tempSample = samples;
+      tempSample[props.id].StepArray[props.index] = 0;
+      setSamples(tempSample);
+      setMute(0);
+      console.log("unmuted " + props.index);
+
+      console.log(samples[props.id].StepArray[props.index]);
+    }
+  };
 
   return (
     <>
       <button
-        className={mute ? "step torq" : "step mute"}
+        className={mute ? "step mute" : "step torq"}
         onClick={() => toggleMute()}
       ></button>
     </>
